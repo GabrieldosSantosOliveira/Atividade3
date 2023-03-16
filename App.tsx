025e0fc0336a0410bce6c68edd0a5652bd7bd504
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-16 13:19:31 -0400

    feat: loaded application fonts

--- App.tsx
+++ App.tsx
@@ -1,53 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  useFonts,
+  Lexend_600SemiBold,
+  Lexend_700Bold,
+} from '@expo-google-fonts/lexend';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
+import { Theme } from './src/styles/Theme';
 export default function App() {
-  const [value, setValue] = useState({ graus: "" });
-  const [temperatura, setTemperatura] = useState({ kelvin: 0, fahrenheit: 0 });
-
-  const handleInputChange = (number: number) => {
-    setValue({ ...value, graus: number.toString() });
-  };
-
-  const calc = () => {
-    let celsius = Number(value.graus);
-    const fahrenheit = celsius / 1.8 + 32;
-    const kelvin = celsius + 273;
-
-    setTemperatura({ fahrenheit: fahrenheit, kelvin: kelvin });
-  };
+  const [isFontsLoaded] = useFonts({
+    Lexend_600SemiBold,
+    Lexend_700Bold,
+  });
+  const { colors } = Theme;
   return (
-    <View style={styles.container}>
-      <Text>Olá</Text>
-      <StatusBar style="auto" />
-      <Text>Temperatura em Fahrenheit: {temperatura.fahrenheit}</Text>
-      <Text>Temperatura em Kelvin: {temperatura.kelvin}</Text>
-
-      <TextInput
-        onChangeText={(e) => {
-          handleInputChange(Number(e));
-        }}
-        keyboardType="numeric"
-        placeholder="Graus"
-        value={value.graus}
-      />
-      <Button
-        onPress={() => {
-          calc();
-        }}
-        title="Calcular temperatura"
-        accessibilityLabel="Calcular Temperatura"
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar backgroundColor={colors.background} />
+      {isFontsLoaded ? <Home /> : <Loading />}
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
